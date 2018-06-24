2f2d546a689286c70e517221e27d2dc7ffe8ca25
Hiroyuki Sano <sh19910711@gmail.com>
2018-06-24 02:10:52 +0000

    notes: paragraphs

--- lib/note/paragraph.rb
+++ lib/note/paragraph.rb
@@ -7,13 +7,29 @@ module Note
     end
 
     def text
-      @content['text']
+      content['text']
     end
 
     def results
-      if res = @content['results']
-        @content['results']['msg'].map{|m|m['data']}.join('\n')
+      if res = content['results']
+        content['results']['msg'].map{|m|m['data']}.join('\n')
       end
+    end
+
+    def date_started
+      content['dateStarted']
+    end
+
+    def date_finished
+      content['dateFinished']
+    end
+
+    def date_updated
+      content['dateUpdated']
+    end
+
+    def editor_mode
+      content['editorMode']
     end
   end
 end
--- spec/note/paragraph_spec.rb
+++ spec/note/paragraph_spec.rb
@@ -7,21 +7,44 @@ describe Note::Paragraph do
       @paragraphs = @note.paragraphs
     end
 
+    let(:p) { @paragraphs[1] }
+    let(:p_noresults) { @paragraphs[0] }
+
     context '.text' do
-      subject { @paragraphs[0].text }
+      subject { p.text }
       it { should include '%sh' }
     end
 
     context '.results' do
       context 'with results' do
-        subject { @paragraphs[1].results }
+        subject { p.results }
         it { should include 'ec2-user' }
       end
 
       context 'with no results' do
-        subject { @paragraphs[0].results }
+        subject { p_noresults.results }
         it { should be_nil }
       end
+    end
+
+    context '.date_started' do
+      subject { p.date_started }
+      it { should eq p.content['dateStarted'] }
+    end
+
+    context '.date_finished' do
+      subject { p.date_finished }
+      it { should eq p.content['dateFinished'] }
+    end
+
+    context '.date_updated' do
+      subject { p.date_updated }
+      it { should eq p.content['dateUpdated'] }
+    end
+
+    context '.editor_mode' do
+      subject { p.editor_mode }
+      it { should eq p.content['editorMode'] }
     end
   end
 end
