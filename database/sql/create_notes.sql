3d67dc021b6698bdea024e9d6f3848fb3a7b589b
Hiroyuki Sano <sh19910711@gmail.com>
2019-01-06 09:57:40 +0000

    rename, retype

--- database/sql/create_notes.sql
+++ database/sql/create_notes.sql
@@ -1,10 +1,10 @@
 create table notes (
   id bigint,
   note_id varchar(255),
-  type varchar(255),
+  note_type varchar(255),
   url varchar(255),
   image varchar(255),
-  is_public varchar(255),
+  is_public tinyint(1),
   created_at timestamp,
   updated_at timestamp
 )
