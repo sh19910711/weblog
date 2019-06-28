31fd124e3db3e12f55fef09f217f9e2046983711
Hiroyuki Sano <sh19910711@gmail.com>
2019-06-28 03:22:08 +0000

    ruby

--- script/run.bash
+++ script/run.bash
@@ -1,4 +1,4 @@
-# usage: HOST=... bash script/run.bash
+export HOST=$(aws ecs list-container-instances --query "containerInstanceArns" --output text --cluster homepage | xargs aws ecs describe-container-instances --cluster homepage --query 'containerInstances[].ec2InstanceId' --output text --container-instances | xargs aws ec2 describe-instances --query 'Reservations[].Instances[0].PrivateIpAddress' --output text --instance-ids)
 export TASK_ID=$(aws ecs list-tasks --cluster homepage --query 'taskArns' --output text)
 export DATABASE_HOST=$HOST
 export DATABASE_PORT=$(aws ecs describe-tasks --cluster homepage --tasks ${TASK_ID} --query 'tasks[].containers[?name==`mysql`].networkBindings[0].hostPort' --output text)
