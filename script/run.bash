5e68eee2aeb84978a94a22552eaecbf4a54f6c24
Hiroyuki Sano <sh19910711@gmail.com>
2019-06-29 02:55:37 +0000

    run

--- script/run.bash
+++ script/run.bash
@@ -1,10 +1,6 @@
-export HOST=$(aws ecs list-container-instances --query "containerInstanceArns" --output text --cluster homepage | xargs aws ecs describe-container-instances --cluster homepage --query 'containerInstances[].ec2InstanceId' --output text --container-instances | xargs aws ec2 describe-instances --query 'Reservations[].Instances[0].PrivateIpAddress' --output text --instance-ids)
 export TASK_ID=$(aws ecs list-tasks --cluster homepage --query 'taskArns' --output text)
-export DATABASE_HOST=$HOST
-export DATABASE_PORT=$(aws ecs describe-tasks --cluster homepage --tasks ${TASK_ID} --query 'tasks[].containers[?name==`mysql`].networkBindings[0].hostPort' --output text)
-export DATABASE_USERNAME=root
-export DATABASE_PASSWORD=mysql
-export SEARCH_HOST=$HOST
-export SEARCH_PORT=$(aws ecs describe-tasks --cluster homepage --tasks ${TASK_ID} --query 'tasks[].containers[?name==`search`].networkBindings[0].hostPort' --output text)
-
+export DATABASE_HOST=database.homepage2
+export DATABASE_PORT=3306
+export SEARCH_HOST=search.homepage2
+export SEARCH_PORT=9200
 PORT=8080 make dev
