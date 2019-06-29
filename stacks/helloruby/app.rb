cf58d173f2de43e1d829e204238092edb6a42161
Hiroyuki Sano <sh19910711@gmail.com>
2019-06-29 08:46:08 +0000

    es 7.2.0

--- stacks/helloruby/app.rb
+++ stacks/helloruby/app.rb
@@ -2,15 +2,14 @@ libdir = File.join(File.dirname(__FILE__), './homepagelib')
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 puts 'require ok'
 
-ENV['DATABASE_HOST'] = 'database.homepage2' # host
-ENV['DATABASE_PORT'] = '3306' # port
+ENV['DATABASE_HOST'] = 'database.homepage2'
 ENV['DATABASE_USERNAME'] = 'root'
 ENV['DATABASE_PASSWORD'] = 'mysql'
+ENV['SEARCH_HOST'] = 'search.homepage2'
 
 require 'database'
 require 'model'
-# require 'content'
-# require 'storage'
+require 'storage'
 
 puts 'second require ok'
 
@@ -23,14 +22,21 @@ def handler(event:, context:)
   when 'query'
     res = ActiveRecord::Base.connection.execute(event['sql'])
     res.to_a(:as => :hash).to_json if res
+  when 'search'
+    es = Storage::Elasticsearch.new
+    es.client.bulk(body: JSON.parse(event['body']))
   end
 end
 
 if __FILE__ == $0
+  body = []
+  body << {index: {_index: 'homepage_note_tags', data: {note_id: '4ba462da-f8f7-4a5a-8366-37e2762eb784', tag: '*algorithm7'}}}
+  body << {index: {_index: 'homepage_note_tags', data: {note_id: '4ba462da-f8f7-4a5a-8366-37e2762eb784', tag: '*algorithm8'}}}
+  body << {index: {_index: 'homepage_note_tags', data: {note_id: '4ba462da-f8f7-4a5a-8366-37e2762eb784', tag: '*algorithm9'}}}
   puts handler(
     event: JSON.parse({
-      'type': 'query',
-      'sql': 'select * from notes'
+      'type': 'search',
+      'body': body.to_json
     }.to_json),
     context: nil
   )
