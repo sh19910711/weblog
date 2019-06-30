0d432d0697a79f493b0946bc8c7c8d2fe6874d8a
Hiroyuki Sano <sh19910711@gmail.com>
2019-06-30 06:31:50 +0000

    delete by query

--- stacks/helloruby/app.rb
+++ stacks/helloruby/app.rb
@@ -30,6 +30,9 @@ def handler(event:, context:)
   when 'search:search'
     es = Storage::Elasticsearch.new
     es.client.search(JSON.parse(event['params'], symbolize_names: true))
+  when 'search:delete_by_query'
+    es = Storage::Elasticsearch.new
+    es.client.delete_by_query(JSON.parse(event['params'], symbolize_names: true))
   end
 end
 
@@ -59,6 +62,39 @@ if __FILE__ == $0
       }
       res = handler(event: JSON.parse(event.to_json), context: nil)
       assert_includes res['hits']['hits'].map { |h| h['_source']['tag'] }, 'Python'
+    end
+
+    def test_search_delete_by_query
+      es = Storage::Elasticsearch.new
+      es.client.index(index: 'test_docs', body: {tag: 'Python'})
+      es.client.index(index: 'test_docs', body: {tag: 'AOJ'})
+      es.client.indices.refresh(index: 'test_docs')
+
+      event = {
+        type: 'search:delete_by_query',
+        params: {
+          index: 'test_docs',
+          body: {
+            query: {
+              match: {
+                tag: 'AOJ'
+              }
+            }
+          }
+        }.to_json,
+      }
+      handler(event: JSON.parse(event.to_json), context: nil)
+      es.client.indices.refresh(index: 'test_docs')
+
+      event = {
+        type: 'search:search',
+        params: {
+          index: 'test_docs',
+        }.to_json,
+      }
+      res = handler(event: JSON.parse(event.to_json), context: nil)
+      assert res['hits']['hits'].map { |h| h['_source']['tag'] }.include?('Python'), 'Python should be included.'
+      assert !res['hits']['hits'].map { |h| h['_source']['tag'] }.include?('AOJ'), 'AOJ should not be included.'
     end
   end
 end
