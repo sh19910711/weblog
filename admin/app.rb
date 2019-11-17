379e85446b41d3c496d3e26543482c921cc9ed99
Hiroyuki Sano <sh19910711@gmail.com>
2019-11-17 00:58:39 +0000

    cut

--- admin/app.rb
+++ admin/app.rb
@@ -10,12 +10,30 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'byebug'
 
+def import_zeppelin
+  s3 = Aws::S3::Resource.new
+  keys = s3.bucket(ENV['S3_BUCKET']).objects(prefix: ENV['S3_PREFIX']).map(&:key)
+  urls = keys.map do |k|
+    "s3://#{ENV['S3_BUCKET']}/#{k}"
+  end
+
+  Model::Note.where(url: urls).each do |note|
+    urls.delete(note.url)
+  end
+
+  Model::Note.transaction do
+    urls.each do |url|
+      puts "import: #{url}"
+      Model::Note.import_zeppelin(url)
+    end
+  end
+end
+
 module Admin
   class App < Sinatra::Base
     enable :prefixed_redirects
 
     configure :development do
-      $logger.info('enable reloader')
       register Sinatra::Reloader
       Pathname.glob('lib/**/*.rb').each {|rb| also_reload rb }
     end
@@ -59,22 +77,7 @@ module Admin
     end
 
     post '/import_zeppelin' do
-      s3 = Aws::S3::Resource.new
-      keys = s3.bucket(ENV['S3_BUCKET']).objects(prefix: ENV['S3_PREFIX']).map(&:key)
-      urls = keys.map do |k|
-        "s3://#{ENV['S3_BUCKET']}/#{k}"
-      end
-
-      Model::Note.where(url: urls).each do |note|
-        urls.delete(note.url)
-      end
-
-      Model::Note.transaction do
-        urls.each do |url|
-          puts "import: #{url}"
-          Model::Note.import_zeppelin(url)
-        end
-      end
+      import_zeppelin
 
       redirect "/"
     end
