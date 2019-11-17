5106795039355918e87f030fb2b636ec55444a9d
Hiroyuki Sano <sh19910711@gmail.com>
2019-11-17 00:59:40 +0000

    chore

--- database/sql/setup.sql
+++ database/sql/setup.sql
@@ -1,19 +1,3 @@
 create database if not exists homepage;
 create user if not exists homepage identified by 'homepage';
 grant select on homepage.* to homepage@'%';
-
-create table homepage.notes (
-  id bigint,
-  note_id varchar(255) not null primary key,
-  note_type varchar(255),
-  url varchar(255),
-  image varchar(255),
-  is_public tinyint(1),
-  --
-  name varchar(255),
-  --
-  created_at timestamp,
-  updated_at timestamp
-)
-ENGINE=InnoDB DEFAULT CHARSET=utf8
-;
